951f629dc932afa91bb728af607ecdc6b82bdb68
Rishabh Jain <rishabh.rj02@gmail.com>
2022-01-25 00:17:09 +0530

    Time: 30 ms (20.29%), Space: 41.5 MB (14.24%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,6 +1,10 @@
 class Solution {
     public int countSubstrings(String s) {
         // credits - Sumeet Sir (Important segment of question)
+        // is sawaal me diagnal par traverse krna seekhna padega
+        // gap jab 0 hoga - palindrome hoga he
+        // gap jab 1 hoga - agr dono same hai to palindrome hoga anytha nahi
+        // gap jab 2 hoga - agr dono same hai aur beech ka sb palindrom hai to poora palindrom hoga vrna nahi hoga (yaha is case me peeche k box check krenge!!!)
         boolean[][] dp = new boolean[s.length()][s.length()];
         int count = 0;
         for(int gap = 0; gap < s.length(); gap++){
