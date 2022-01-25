4640ec7d10a9dc7400b4f66973fcf534b74dd5b8
Rishabh Jain <rishabh.rj02@gmail.com>
2022-01-25 09:39:26 +0530

    Time: 570 ms (12.83%), Space: 106.7 MB (14.34%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -2,16 +2,7 @@ class Solution {
     public String longestPalindrome(String s) { 
         int x = 0;
         int y = 0;
-        // same as previous category to find the count of longest substring palindrome
-        // note: dp kaa matlab:
-        // dp x axis = end points
-        // dp y axis = start points
-        // sare gap diagonals pr check kro ki palindrome hai ki nahi
-        // gap = 0 diagonal - ye toh palindrome honge hi, since start aur end same hai 
-        // gap = 1 diagonal - ye tabhi palindrome honge jab start aur end character same honge
-        // gap > 1 diagonal - ye tabhi palindrom honge jab :
-                                // 1) start aur end character same honge aur 
-                                // 2) beech ki puri string palindrome ho - i + 1, j - 1 pr ho calculate kia tha vo
+    
         boolean[][] dp = new boolean[s.length()][s.length()];
         for(int gap = 0; gap < dp.length; gap++){
             for(int i = 0, j = gap; j < dp.length; i++, j++){
