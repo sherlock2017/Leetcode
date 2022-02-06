d0185c289c056a72ed40aa2ec08f93d823c5831a
Rishabh Jain <rishabh.rj02@gmail.com>
2022-02-06 04:05:11 -0800

    Time: 2 ms (77.77%), Space: 55.8 MB (7.39%) - LeetHub

---
+++ 370-range-addition/370-range-addition.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        
+        int[] res = new int[length];
+        
+        // create impact with each update query
+        for(int i = 0; i < updates.length; i++){
+            int starti = updates[i][0];
+            int endi = updates[i][1];
+            int inc = updates[i][2];
+            
+            res[starti] += inc;
+            if(endi + 1 < length){
+                res[endi + 1] -= inc;
+            }
+        }
+        
+        // calculate prefix sum
+        int sum = 0;
+        for(int i = 0; i < length; i++){
+            sum += res[i];
+            res[i] = sum;
+        }
+        
+        return res;
+    }
+}
