e2709bb603c94f4e7851bc2b3e27755a67ec2095
Rishabh Jain <rishabh.rj02@gmail.com>
2022-02-16 21:26:26 +0530

    Time: 61 ms (46.94%), Space: 57.9 MB (62.74%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,40 +1,19 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        TreeMap<String, List<String>> map = new TreeMap<>();
-        
+        List<List<String>> suggestions = new ArrayList<>();
         Arrays.sort(products, (a, b) -> a.compareTo(b));
-        
-        for(String p : products){
-            System.out.print(p+" ");
-        }
-        System.out.println();
-        
         for(int j = 1; j <= searchWord.length(); j++){
-            
                 String prefix = searchWord.substring(0, j);
-                System.out.println(prefix);
-                
                 List<String> list = new ArrayList<>();
                 for(String product: products){
                     if(product.startsWith(prefix)){
                         if(list.size() < 3){
                             list.add(product);   
-                        }
-                        
+                        }  
                     }
                 }
-
-                map.put(prefix, list);
+                suggestions.add(list);
         }
-          
-        System.out.println(map);
-        
-        List<List<String>> suggestions = new ArrayList<>();
-        for(String prefix : map.keySet()){
-            List<String> list = map.get(prefix);
-            suggestions.add(list);
-        }
-        
         return suggestions;
     }
 }
